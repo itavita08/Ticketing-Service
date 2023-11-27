8be0fbb132b2b16e4e7d7cf7361b77d3934b8dff
itavita08 <dudwns10000@gmail.com>
2023-11-28 00:42:47 +0900

    refactor: Request, Response Dto 수정
    dto에서 어플리케이션을 구동하는데 필요없는 필드 삭제

--- src/main/java/com/example/ticketing/controller/response/UserJoinResponse.java
+++ src/main/java/com/example/ticketing/controller/response/UserJoinResponse.java
@@ -9,13 +9,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public class UserJoinResponse {
 
-    private Integer id;
     private String username;
     private UserRole role;
 
     public static UserJoinResponse fromUser(User user){
         return new UserJoinResponse(
-                user.getId(),
                 user.getUsername(),
                 user.getRole()
         );
